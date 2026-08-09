90cdd6d4b3be0b7bea847aa93e6ed9507ba8e86d
gauravPanditr <gauravpandit7856@gmail.com>
2026-08-09 13:16:24 +0530

    added middleware in events

--- src/routes/review.routes.ts
+++ src/routes/review.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getReviewsController } from "../controller/review.controller.js";
+import { requireAuth } from "../middleware/auth.middlware.js";
 
 
 const router = Router();
 
-router.get("/", getReviewsController);
+router.get("/",requireAuth, getReviewsController);
 
 export default router;
--- src/routes/webhook.routes.ts
+++ src/routes/webhook.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { githubWebhookController } from "../controller/webhook.controller.js";
+import { requireAuth } from "../middleware/auth.middlware.js";
 
 const router = Router();
 
 router.post(
   "/github",
+  requireAuth,
   githubWebhookController
 );
 
